0ec086364a1d3439348962ff8f6de756509aa0cf
Quelvim Uiliam Barreto dos Santos <quelvimbarreto@gmail.com>
2023-04-26 08:26:57 -0300

    update

--- suila/assets/About-9633f846.js
+++ suila/assets/About-9633f846.js
@@ -1,1 +1,25 @@
-import{j as a}from"./index-07f5077d.js";const e="/assets/woman2-45f59622.jpg";function o(){return a.jsxs("div",{className:"about",id:"about",children:[a.jsxs("div",{className:"about_text",children:[a.jsx("h1",{children:"Sobre mim"}),a.jsx("p",{children:'Olá! Sou Suila Gomes Alencar, uma fotógrafa apaixonada pelo que faço. Minha missão é criar imagens que transmitam emoções, contem histórias e sejam verdadeiramente memoráveis. Meu estilo é "xxxxxx", e estou comprometida em capturar momentos especiais para meus clientes de forma única e autêntica.'})]}),a.jsx("div",{className:"about_img",children:a.jsx("img",{src:e,alt:"woman"})})]})}export{o as default};
+import { j as a } from "./index-07f5077d.js";
+const e = "suila/assets/woman2-45f59622.jpg";
+function o() {
+  return a.jsxs("div", {
+    className: "about",
+    id: "about",
+    children: [
+      a.jsxs("div", {
+        className: "about_text",
+        children: [
+          a.jsx("h1", { children: "Sobre mim" }),
+          a.jsx("p", {
+            children:
+              'Olá! Sou Suila Gomes Alencar, uma fotógrafa apaixonada pelo que faço. Minha missão é criar imagens que transmitam emoções, contem histórias e sejam verdadeiramente memoráveis. Meu estilo é "xxxxxx", e estou comprometida em capturar momentos especiais para meus clientes de forma única e autêntica.',
+          }),
+        ],
+      }),
+      a.jsx("div", {
+        className: "about_img",
+        children: a.jsx("img", { src: e, alt: "woman" }),
+      }),
+    ],
+  });
+}
+export { o as default };
