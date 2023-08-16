f033f6446b61eff2246e02dba43a52e6170da733
Quelvim Uiliam Barreto dos Santos <quelvimbarreto@gmail.com>
2023-08-16 10:23:19 -0300

    Newsletter configuration

---
+++ rochabarreto/assets/js/animation.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+  Tu.tScroll({
+    "t-element": ".fadeLeft",
+    "t-animate": "fadeLeft",
+  });
+  Tu.tScroll({
+    "t-element": ".fadeRight",
+    "t-animate": "fadeRight",
+  });
+  Tu.tScroll({
+    "t-element": ".fadeUp",
+    "t-animate": "fadeUp",
+  });
+};
